11f31c808136d6f50cdfd28edc76756e4aada624
aishwarya <aishwaryasuresh020@gmail.com>
2026-04-13 10:16:56 +0530

    Add unit tests for payment processing in AppTest
    Added unit tests for payment processing scenarios including success, invalid amount, and invalid card number.

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,1 +1,25 @@
+package com.example;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void testPaymentSuccess() {
+        App app = new App();
+        assertTrue(app.processPayment(1000, "1234567812345678"));
+    }
+
+    @Test
+    public void testInvalidAmount() {
+        App app = new App();
+        assertFalse(app.processPayment(-500, "1234567812345678"));
+    }
+
+    @Test
+    public void testInvalidCard() {
+        App app = new App();
+        assertFalse(app.processPayment(1000, "1234"));
+    }
+}
